486400d756aa06292217bf352421611901432cba
shinokun <hypatia.tile02021@gmail.com>
2026-07-05 09:34:48 +0900

    feat: zsh config
    Add function to make single private repository and clone

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -3,3 +3,10 @@ repos() {
   repo="$(ghq list | fzf)" || return
   cd "$(ghq root)/$repo"
 }
+
+create_repo_mit() {
+  local repo_name="hypatia-tile/$1"
+  gh repo create "${repo_name}" --add-readme --license=MIT --private \
+    && ghq get -p "${repo_name}"
+}
+
